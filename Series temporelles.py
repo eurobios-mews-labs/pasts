6cb926cca867c357caca501169f263d90cac95b1
dorian.collot <dorian.collot@eurobios.com>
2023-01-25 15:50:37 +0100

    Initial commit

---
+++ Series temporelles.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 15:11:00 2023
+
+@author: dcollot
+"""
+
+for k, t in SC.items():
+    plt.scatter(k,t)
+plt.show()
+test
+
+
