c64bd13b968c37ec4c8112fca18423a4b754c73d
Rawya Zreik <rzreik@eurobios.com>
2023-03-06 16:03:38 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='seriestemporelles',
+    author='eurobios scientific computing branch',
+    # license='MIT',
+)
